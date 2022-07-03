82ffc104ac922e32c6a728517792af0ef94c31b1
Marijn van Aerle <marijn.vanaerle@gmail.com>
2022-07-03 13:51:44 +0200

    Support new rabobank filename

--- beancount_rabobank/rabobank.py
+++ beancount_rabobank/rabobank.py
@@ -9,7 +9,8 @@ from beancount.core import amount
 from beancount.core.number import D
 
 RABOBANK_FILE_PATTERNS = [
-    r"CSV_A_(\d{4}\d{2}\d{2})_\d{6}\.csv"
+    r"CSV_A_(\d{4}\d{2}\d{2})_\d{6}\.csv",
+    r"CSV_A_\w{2}\d{2}\w{4}\d{10}_\w{3}_\d{8}_(\d{4}\d{2}\d{2})\.csv"
 ]
 
 RABOBANK_HEADER_PATTERNS = [
